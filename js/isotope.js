74582faee84a84ea0a31e87cdb3fe096d9265d5f
Reza A'masyi <mnurrreza@gmail.com>
2023-12-05 12:39:35 +0700

    post(garden+projects): Finitude of Life and Org-Journal management

--- js/isotope.js
+++ js/isotope.js
@@ -1,28 +1,28 @@
 // Solution working for a multiple buttons checked at a time
 // https://codepen.io/desandro/pen/JGKyrY
 //
-var $blogposts = $("#blogposts");
-$blogposts.isotope({ itemSelector: ".col" });
-var $checkboxes = $("#filters input");
+var $blogposts = $('#blogposts');
+$blogposts.isotope({itemSelector : '.col'});
+var $checkboxes = $('#filters input');
 
-$checkboxes.change(function () {
-  var inclusives = [];
-  $checkboxes.each(function (i, elem) {
-    if (elem.checked) {
-      inclusives.push(elem.id);
-    }
-  });
-  var filterValue = inclusives.length ? inclusives.join(", ") : "*";
-  $blogposts.isotope({ filter: filterValue });
+$checkboxes.change( function() {
+    var inclusives = [];
+    $checkboxes.each( function( i, elem ) {
+      if ( elem.checked ) {
+        inclusives.push( elem.id );
+      }
+    });
+    var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+    $blogposts.isotope({ filter: filterValue })
 });
 
 // Solution working for a single button checked at a time
 // https://codepen.io/desandro/pen/lxzDA
 //
-// var $blogposts = $('#blogposts');
+// var $blogposts = $('#blogposts'); 
 // $blogposts.isotope({
 //   itemSelector : '.col'
-// });
+// }); 
 // $('#filters').on( 'click', 'button', function() {
 //     var selector = $(this).attr('data-filter');
 //     $blogposts.isotope({ filter: selector });
